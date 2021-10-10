1df2c88fb5f5ac4c7aef4a961b8c38a0f18cd928
josh <joecody@iu.edu>
2021-10-10 12:33:51 -0400

    added labels, removed test prints, and fixed indexing issue

--- Ethermine_data/main.py
+++ Ethermine_data/main.py
@@ -18,32 +18,27 @@ for j in config['info']['id']:
     workerIDs.append(j)
     apiCall.append("https://api.ethermine.org/miner/" + j + "/dashboard")
 
-timeGauge = Gauge('time', 'the time')
-reportedHashrateGauge = Gauge('reported_hashrate', 'the reported hashrate')
-currentHashrateGauge = Gauge('current_hashrate', 'the current hashrate')
-validSharesGauge = Gauge('valid_shares', 'the number of valid shares')
-invalidSharesGauge = Gauge('invalid_shares', 'the number of invalid shares')
-staleSharesGauge = Gauge('stale_shares', 'the number of stale shares')
-activeWorkersGauge = Gauge('active_workers', 'the number of active workers')
+timeGauge = Gauge('time', 'the time', ["workerid"])
+reportedHashrateGauge = Gauge('reported_hashrate', 'the reported hashrate', ["workerid"])
+currentHashrateGauge = Gauge('current_hashrate', 'the current hashrate', ["workerid"])
+validSharesGauge = Gauge('valid_shares', 'the number of valid shares', ["workerid"])
+invalidSharesGauge = Gauge('invalid_shares', 'the number of invalid shares', ["workerid"])
+staleSharesGauge = Gauge('stale_shares', 'the number of stale shares', ["workerid"])
+activeWorkersGauge = Gauge('active_workers', 'the number of active workers', ["workerid"])
 
 while True:
     for i in apiCall:
+        
         dat = requests.get(i)
         jsonDat = json.loads(dat.text)
         dat2 = jsonDat["data"]
         rawData = dat2["statistics"]
-        #stats = json.load(rawData)
-        if len(rawData) > 0:
-            print(rawData[0])
-        j = 0
         for j in rawData:
-            timeGauge.labels(workerIDs[apiCall.index(i)]).set(rawData[j]['time'])
-            print("rawData[0] \n", rawData[0])
-            reportedHashrateGauge.labels(workerIDs[apiCall.index(i)]).set(rawData[j]['reportedHashrate'])
-            currentHashrateGauge.labels(workerIDs[apiCall.index(i)]).set(rawData[j]['currentHashrate'])
-            validSharesGauge.labels(workerIDs[apiCall.index(i)]).set(rawData[j]['validShares'])
-            invalidSharesGauge.labels(workerIDs[apiCall.index(i)]).set(rawData[j]['invalidShares'])
-            staleSharesGauge.labels(workerIDs[apiCall.index(i)]).set(rawData[j]['staleShares'])
-            activeWorkersGauge.labels(workerIDs[apiCall.index(i)]).set(rawData[j]['activeWorkers'])
-            j += 1
+            timeGauge.labels(workerIDs[apiCall.index(i)]).set(j['time'])
+            reportedHashrateGauge.labels(workerIDs[apiCall.index(i)]).set(j['reportedHashrate'])
+            currentHashrateGauge.labels(workerIDs[apiCall.index(i)]).set(j['currentHashrate'])
+            validSharesGauge.labels(workerIDs[apiCall.index(i)]).set(j['validShares'])
+            invalidSharesGauge.labels(workerIDs[apiCall.index(i)]).set(j['invalidShares'])
+            staleSharesGauge.labels(workerIDs[apiCall.index(i)]).set(j['staleShares'])
+            activeWorkersGauge.labels(workerIDs[apiCall.index(i)]).set(j['activeWorkers'])
     time.sleep(int(config['info']['scrape_interval']))
